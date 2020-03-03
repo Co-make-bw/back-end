b3d4fac57440486051f833bce559c8e4e7659dc9
CAM603 <cameronhawley@comcast.net>
2020-03-03 18:33:39 -0500

    ADD tests

--- api/server.test.js
+++ api/server.test.js
@@ -8,5 +8,13 @@ describe('server', () => {
         it('should run the tests', () => {
             expect(true).toBe(true)
         })
+        it('should use the testing environment', () => {
+            expect(process.env.DB_ENV).toBe('testing')
+        })
+        it('should return status 200 after GET', async () => {
+            const res = await request(server).get('/')
+
+            expect(res.status).toBe(200)
+        })
     })
 })
