a23ebfabc63c9dd166fba49304fa32d3511a429c
CAM603 <cameronhawley@comcast.net>
2020-03-04 14:41:51 -0500

    ADD tests

--- states/states.test.js
+++ states/states.test.js
@@ -2,38 +2,40 @@ const request = require('supertest');
 const server = require('../api/server');
 const db = require('../data/dbConfig');
 // for decoding the token and easily extracting the id from the payload
-const jsonwebtoken = require("jsonwebtoken");
+// const jsonwebtoken = require("jsonwebtoken");
 // for hashing the password successfully when we create users
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 
 // our global object for storing auth information
-let auth = {};
+let token;
 
 describe('states router', () => {
     beforeEach(async () => {
         await db('users').del()
-
-        const hash = await bcrypt.hash('secret', 1)
-        await request(server).post('/api/auth/register')
-            .send({username: 'Sonic', password: hash})
     })
     it('should not allow access without token', async () => {
         const res = await request(server).get('/api/states');
 
         expect(res.status).toBe(401)
     })
-    // it('should allow access with token', async () => {
-    //     const res = await request(server).get('/api/states')
-    //         .auth('username', 'password')
-    //         .set('Accept', 'application/json')
-        
-    //     expect(res.status).toBe(200)
-    // })
-})
-        // it('should allow access with token', async () => {
-        //     await request(server).post('/api/auth/register')
-        //         .send({username: 'Sonic', password: 'rings'})
+    it('should allow access with token', async () => {
+        await request(server).post('/api/auth/register')
+                .send({username: 'Sonic', password: 'rings'})
             
-        //     await request(server).post('/api/auth/login')
-        //         .send({username: 'Sonic', password: 'rings'})
-        // })
+        let res = await request(server).post('/api/auth/login')
+                .send({username: 'Sonic', password: 'rings'})
+        
+        token = res.body.token
+
+        let res2 = await request(server).get('/api/states')
+            .set('Authorization', token)
+        
+        expect(res2.status).toBe(200)
+    })
+    it('should be an array containing 50 states', async () => {
+        let res = await db('states')
+
+        expect(Array.isArray(res)).toBe(true)
+        expect(res).toHaveLength(50)
+    })
+})
