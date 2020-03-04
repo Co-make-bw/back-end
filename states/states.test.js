4dc608112317c002c69e137e50ba10e50b178901
CAM603 <cameronhawley@comcast.net>
2020-03-04 12:27:19 -0500

    ADD posted_by field

--- states/states.test.js
+++ states/states.test.js
@@ -1,10 +1,21 @@
 const request = require('supertest');
 const server = require('../api/server');
 const db = require('../data/dbConfig');
+// for decoding the token and easily extracting the id from the payload
+const jsonwebtoken = require("jsonwebtoken");
+// for hashing the password successfully when we create users
+const bcrypt = require("bcrypt");
+
+// our global object for storing auth information
+let auth = {};
 
 describe('states router', () => {
     beforeEach(async () => {
         await db('users').del()
+
+        const hash = await bcrypt.hash('secret', 1)
+        await request(server).post('/api/auth/register')
+            .send({username: 'Sonic', password: hash})
     })
     it('should not allow access without token', async () => {
         const res = await request(server).get('/api/states');
